e276f9fcd1086c6521804046078672a9b82ca1ae
jasonwalsh <rightlag@gmail.com>
2018-07-15 19:23:59 -0400

    Update initialize script
    The Dockerfile ENTRYPOINT is the first process started in userspace.
    This process runs the `run_worker.sh` script which spawns a celery
    worker process and starts the flask application.
    
    See #6
    
    Signed-off-by: jasonwalsh <rightlag@gmail.com>

--- scripts/run_worker.sh
+++ scripts/run_worker.sh
@@ -6,7 +6,22 @@ set -e
 
 application="$1"
 
-# Start a celery worker process.
-/usr/local/bin/celery -A "$application" worker > /dev/null 2>&1 &
+panic() {
+  echo "$1"
+  exit 1
+}
 
-exit 0
+if [ -z "$application" ]; then
+  panic 'main: a celery project is required'
+fi
+
+# Start a celery worker process.
+/usr/local/bin/celery -A "$application" worker -D --pidfile "$HOME/%n.pid"
+
+# Start the flask application.
+/usr/local/bin/flask run
+
+# Terminate all worker processes forked by celery.
+trap $(ps aux | grep -v grep | grep 'celery worker' | awk '{print $2}' | xargs kill -9) SIGINT
+
+exit "$?"
