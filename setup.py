ec9f89dc75e5139793ea65930583b1778bddaeea
jasonwalsh <rightlag@gmail.com>
2018-08-02 19:25:24 -0400

    Add provisioning endpoint
    Signed-off-by: jasonwalsh <rightlag@gmail.com>

--- setup.py
+++ setup.py
@@ -1,30 +1,34 @@
 from setuptools import (
-	find_packages,
-	setup,
+    find_packages,
+    setup,
 )
 
 
 setup(
-	author='hey-mako',
-	include_package_data=True,
-	install_requires=[
-		'Flask==1.0.2',
-	],
-	license='MIT',
-	name='botops',
-	packages=find_packages(
-		exclude=[
-			'*.tests',
-			'*.tests.*',
-			'tests',
-			'tests.*',
-		]
-	),
-	setup_requires=[
-		'pytest-runner',
-	],
-	tests_require=[
-		'pytest',
-	],
-	url='https://github.com/hey-mako/heroku-botops'
+    author='hey-mako',
+    include_package_data=True,
+    install_requires=[
+        'ansible==2.6.2',
+        'celery==4.2.1',
+        'pymongo==3.7.1',
+        'redis==2.10.6',
+        'Flask==1.0.2'
+    ],
+    license='MIT',
+    name='botops',
+    packages=find_packages(
+        exclude=[
+            '*.tests',
+            '*.tests.*',
+            'tests',
+            'tests.*',
+        ]
+    ),
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    url='https://github.com/hey-mako/heroku-botops'
 )
--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,69 @@
-from flask import Flask
+import json
+import os
+import subprocess
+
+from celery import Celery
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 
 application = Flask(__name__)
+application.config.update(
+    BROKER_URL=os.getenv('BROKER_URL'),
+    CELERY_RESULT_BACKEND=os.getenv('CELERY_RESULT_BACKEND')
+)
 
-import src.views
+# TODO: Use an environment variable for the connection URI.
+client = MongoClient('mongo', 27017)
+database = client.heroku
+
+
+def make_celery(application):
+    celery = Celery(
+        application.import_name,
+        backend=application.config['CELERY_RESULT_BACKEND'],
+        broker=application.config['BROKER_URL']
+    )
+    celery.conf.update(application.config)
+
+    class ContextTask(celery.Task):
+
+        def __call__(self, *args, **kwargs):
+            with application.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+
+celery = make_celery(application)
+
+
+@celery.task
+def provision():
+    command = '/usr/local/bin/ansible-playbook -i localhost, scripts/main.yml'.split()
+    result = subprocess.run(
+        command,
+        check=True,
+        env={'ANSIBLE_STDOUT_CALLBACK': 'json'},
+        stdout=subprocess.PIPE
+    )
+    return json.loads(result.stdout.decode('UTF-8'))
+
+
+@application.route('/heroku/resources', methods=['POST'])
+def resources():
+    data = request.get_json(force=True)
+    collection = database.resources
+    document = collection.find_one(filter={'uuid': data['uuid']})
+    if document is not None:
+        task = provision.AsyncResult(document['task_id'])
+        return jsonify({
+            'result': task.result,
+            'state': task.state
+        }), 200
+    result = provision.delay()
+    # Add the `task_id` key to the `data` object.
+    data['task_id'] = result.id
+    collection.insert_one(data)
+    return jsonify({'id': result.id}), 202
