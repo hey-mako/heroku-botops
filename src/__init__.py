a1c17f09fa52bf2393a4dd2cf698a3584e004978
jasonwalsh <rightlag@gmail.com>
2018-08-03 11:38:57 -0400

    Add remaining provisioning endpoints
    Signed-off-by: jasonwalsh <rightlag@gmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,8 @@ import json
 import os
 import subprocess
 
+from base64 import b64decode
+from functools import wraps
 from celery import Celery
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
@@ -12,6 +14,26 @@ application.config.update(
     BROKER_URL=os.getenv('BROKER_URL'),
     CELERY_RESULT_BACKEND=os.getenv('CELERY_RESULT_BACKEND')
 )
+
+filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'addon-manifest.json')
+
+with open(filename) as f:
+    manifest = json.load(f)
+
+
+def login_required(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        header = request.headers.get('Authorization')
+        if header is None:
+            return '', 401
+        header = header.split()[-1]
+        user, password = b64decode(header.encode()).decode().split(':')
+        if not (user == manifest['id'] and password == manifest['api']['password']):
+            return '', 401
+        return f(*args, **kwargs)
+    return inner
+
 
 # TODO: Use an environment variable for the connection URI.
 client = MongoClient('mongo', 27017)
@@ -52,10 +74,11 @@ def provision():
 
 
 @application.route('/heroku/resources', methods=['POST'])
+@login_required
 def resources():
     data = request.get_json(force=True)
     collection = database.resources
-    document = collection.find_one(filter={'uuid': data['uuid']})
+    document = collection.find_one({'uuid': data['uuid']})
     # See https://goo.gl/3Y83zz
     if document is not None:
         task = provision.AsyncResult(document['task_id'])
@@ -68,3 +91,31 @@ def resources():
     data['task_id'] = result.id
     collection.insert_one(data)
     return jsonify({'id': result.id}), 202
+
+
+@application.route('/heroku/resources/<heroku_uuid>', methods=['PUT'])
+@login_required
+def plan(heroku_uuid):
+    data = request.get_json(force=True)
+    collection = database.resources
+    # Returns the original document before it was updated, or None if no
+    # document matches.
+    document = collection.find_one_and_update(
+        {'task_id': heroku_uuid},
+        {'$set': {'plan': data['plan']}}
+    )
+    if document is None:
+        return '', 404
+    return '', 200
+
+@application.route('/heroku/resources/<heroku_uuid>', methods=['DELETE'])
+@login_required
+def delete(heroku_uuid):
+    collection = database.resources
+    filter = {'task_id': heroku_uuid}
+    collection.find_one_and_delete(filter)
+    count = collection.count(filter)
+    if count != 0:
+        return '', 400
+    return '', 204
+
